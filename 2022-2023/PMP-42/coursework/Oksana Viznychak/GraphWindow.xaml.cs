7fc4b8b6160111c34ddae308355d397e3df130d3
oksanaViznychak <136252161+oksanaViznychak@users.noreply.github.com>
2023-06-11 22:56:02 +0300

    Add files via upload

---
+++ 2022-2023/PMP-42/coursework/Oksana Viznychak/GraphWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using BoxCountingFractal.Logic;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows;
+using ZedGraph;
+
+namespace BoxCountingFractal
+{
+    public partial class GraphWindow:Window
+    {
+        private GraphPane graphPane;
+        
+        public GraphWindow()
+        {
+            InitializeComponent();
+
+            graphPane = graph.GraphPane;
+        }
+
+       
+        public void DrawApproximation(IList<Tuple<double, double>> points)
+        {
+            graphPane.Title.Text = "Графік апроксимації";
+            graphPane.YAxis.Title.Text = "N(eps)";
+            graphPane.XAxis.Title.Text = "eps";
+
+            graphPane.CurveList.Clear();
+
+            // Малюємо крапки
+            PointPairList list = new PointPairList();
+
+            foreach (Tuple<double, double> point in points)
+            {
+                list.Add(point.Item1, point.Item2);
+            }
+
+            LineItem scatterLine = graphPane.AddCurve("", list, Color.DarkBlue, SymbolType.Triangle);
+            scatterLine.Line.IsVisible = false;
+            scatterLine.Symbol.Fill.Color = Color.DarkBlue;
+            scatterLine.Symbol.Fill.Type = FillType.Brush;
+
+            // Малюємо апроксимацію
+            LessSquare.GetCoefficient(points, out double k, out double b);
+
+            PointPairList fList = new PointPairList();
+            double xMin = ((int)points[0].Item1) - 1;
+            double xMax = ((int)points[points.Count - 1].Item1) + 1;
+            double yMin = ((int)points[0].Item2) - 1;
+            double yMax = ((int)points[points.Count - 1].Item2) + 1;
+
+            for (double x = xMin; x < xMax; x++)
+            {
+                fList.Add(x, LinearFunction(x, k, b));
+            }
+
+            LineItem line = graphPane.AddCurve("", fList, Color.Red, SymbolType.None);
+
+            graphPane.XAxis.Scale.Min = xMin;
+            graphPane.XAxis.Scale.Max = xMax;
+            graphPane.YAxis.Scale.Min = yMin;
+            graphPane.YAxis.Scale.Max = yMax;
+
+            graph.AxisChange();
+            graph.Invalidate();
+        }
+        public void DrawRelation(IList<Tuple<double, double>> points, string title, string yAxisTitle, string xAxisTitle)
+        {
+            graphPane.Title.Text = title;
+            graphPane.YAxis.Title.Text = yAxisTitle;
+            graphPane.XAxis.Title.Text = xAxisTitle;
+
+            graphPane.CurveList.Clear();
+
+            // Малюємо крапки
+            PointPairList list = new PointPairList();
+
+            foreach (Tuple<double, double> point in points)
+            {
+                list.Add(point.Item1, point.Item2);
+            }
+
+            LineItem scatterLine = graphPane.AddCurve("", list, Color.DarkBlue, SymbolType.Triangle);
+            scatterLine.Symbol.Fill.Color = Color.DarkBlue;
+            scatterLine.Symbol.Fill.Type = FillType.Brush;
+
+            graph.AxisChange();
+            graph.Invalidate();
+        }
+        public void DrawLayers(IList<Tuple<Tuple<double, double>, double>> points, string title, string yAxisTitle, string xAxisTitle)
+        {
+            graphPane.Title.Text = title;
+            graphPane.YAxis.Title.Text = yAxisTitle;
+            graphPane.XAxis.Title.Text = xAxisTitle;
+
+            graphPane.CurveList.Clear();
+
+            PointPairList list = new PointPairList();
+            string[] names = new string[points.Count];
+
+            int i = 0;
+            foreach (Tuple<Tuple<double, double>, double> point in points)
+            {
+                list.Add(point.Item1.Item1, point.Item2);
+
+                names[i] = string.Format("от {0} до {1} ", Math.Round(point.Item1.Item1 * 1E+5, 3), Math.Round(point.Item1.Item2 * 1E+5, 3));
+                i++;
+            }
+
+            BarItem curve = graphPane.AddBar("", list, Color.Blue);
+
+            graphPane.BarSettings.MinClusterGap = 5;
+            graphPane.XAxis.Type = AxisType.Text;
+            graphPane.XAxis.Scale.TextLabels = names;
+            graphPane.XAxis.Scale.FontSpec.Size = 10;
+
+            graph.AxisChange();
+            graph.Invalidate();
+        }
+        private double LinearFunction(double x, double k, double b)
+        {
+            return k * x + b;
+        }
+    }
+}
---
+++ 2022-2023/PMP-42/coursework/Oksana Viznychak/MainWindow.xaml.cs
@@ -0,0 +1,166 @@
+﻿using BoxCountingFractal.Logic;
+using Microsoft.Win32;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace BoxCountingFractal
+{
+   
+    public partial class MainWindow : Window
+    {
+        private string imageFilepath;
+        private FractalDimension fractalDimension;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            fractalDimension = new FractalDimension();
+            imageFilepath = string.Empty;
+            thresholdBlackColorNumeric.Value = fractalDimension.BlackBoundary;
+
+            SetEnablePanels(false);
+        }
+        private void SetEnablePanels(bool isEnable)
+        {
+            CapacitiveDimensionPanel.IsEnabled = MinkowskiDimensionsPanel.IsEnabled =
+                RenyiSpectrumPanel.IsEnabled = MultifactorSpectrumPanel.IsEnabled = isEnable;
+        }
+        private void LoadImageButtonClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                InitialDirectory = "C:\\",
+                Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG",
+                RestoreDirectory = true
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                // Отримати шлях до вказаного файлу
+                imageFilepath = openFileDialog.FileName;
+
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(imageFilepath);
+                bitmap.EndInit();
+
+                mainImage.Source = bitmap;
+
+                thresholdBlackColorNumeric.IsEnabled = true;
+                thresholdBlackColorNumeric.Maximum = (int?)(Math.Min(mainImage.Source.Width, mainImage.Source.Height) / 16);
+                SetEnablePanels(true);
+            }
+        }
+        private void ThresholdCalculateClick(object sender, RoutedEventArgs e)
+        {
+            fractalDimension.BlackBoundary = (int)thresholdBlackColorNumeric.Value;
+            thresholdResult.Text = fractalDimension.CalculateCapacitiveDimension(imageFilepath).ToString();
+
+            GraphWindow graphWindow = new GraphWindow();
+            graphWindow.DrawApproximation(fractalDimension.CDPoints);
+            graphWindow.Show();
+        }
+        private void CellSizeCalculateClick(object sender, RoutedEventArgs e)
+        {
+            int epsilon = (int)cellSizeInput.Value;
+
+            cellSizeResult.Text = fractalDimension.CalculateMinkowskiDimensionForGrayscaleImages(imageFilepath, epsilon).ToString();
+        }
+
+        private void ApplyThresholdClick(object sender, RoutedEventArgs e)
+        {
+            SetBlackBoundaryToImageBox();
+        }
+        private void DependenceDeltaOnCellSizeClick(object sender, RoutedEventArgs e)
+        {
+            int epsilon = (int)cellSizeInput.Value;
+
+            fractalDimension.CalculateMinkowskiDimensionForGrayscaleImages(imageFilepath);
+
+            GraphWindow graphWindow = new GraphWindow();
+            graphWindow.DrawRelation(fractalDimension.MDPoints, "Графік залежності A delta від розміру осередку розбиття", "A delta", "eps");
+            graphWindow.Show();
+        }
+        private void SetBlackBoundaryToImageBox()
+        {
+            int blackBoundary = (int)thresholdBlackColorNumeric.Value;
+            Bitmap image = new Bitmap(Image.FromFile(imageFilepath));
+            Bitmap newImage = new Bitmap(image.Width, image.Height);
+
+            for (int x = 0; x < newImage.Width; x++)
+            {
+                for (int y = 0; y < newImage.Height; y++)
+                {
+                    Color pixel = image.GetPixel(x, y);
+
+                    if (pixel.R <= blackBoundary && pixel.G <= blackBoundary && pixel.B <= blackBoundary)
+                    {
+                        newImage.SetPixel(x, y, Color.Black);
+                    }
+                    else
+                    {
+                        newImage.SetPixel(x, y, Color.White);
+                    }
+                }
+            }
+
+            mainImage.Source = ToBitmapImage(newImage);
+        }
+        public BitmapImage ToBitmapImage(Bitmap bitmap)
+        {
+            using (var memory = new MemoryStream())
+            {
+                bitmap.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                bitmapImage.Freeze();
+
+                return bitmapImage;
+            }
+        }
+        private void CalculateRenyiClick(object sender, RoutedEventArgs e)
+        {
+            fractalDimension.QMin = (int)qMinInput.Value;
+            fractalDimension.QMax = (int)qMaxInput.Value;
+            fractalDimension.CalculateRenyiSpectre(imageFilepath);
+
+            GraphWindow graphWindow = new GraphWindow();
+            graphWindow.DrawRelation(fractalDimension.SRPoints, "Графік залежності D(q) від значення q", "D(q)", "q");
+            graphWindow.Show();
+        }
+        private void CalculateBoundsClick(object sender, RoutedEventArgs e)
+        {
+            fractalDimension.PrecalculateAlpaMinAlpaMax(imageFilepath);
+
+            fractalMinInput.Text = String.Format("{0} * E-5", Math.Round(fractalDimension.AlphaMin * 1E+5, 3));
+            fractalMaxInput.Text = String.Format("{0} * E-5", Math.Round(fractalDimension.AlphaMax * 1E+5, 3));
+
+            splitStepInput.IsEnabled = calculateRenyiMF.IsEnabled = true;
+        }
+        private void CalculateMultifractalClick(object sender, RoutedEventArgs e)
+        {
+            fractalDimension.MFEpsilon = Convert.ToDouble(splitStepInput.Text);
+
+            fractalDimension.CalculateMultufractalWithLocalDensityFunction();
+
+            //малюємо графік
+            GraphWindow graphWindow = new GraphWindow();
+            graphWindow.DrawLayers(fractalDimension.MFPoints, "Гістограма спектрів", "Ємнісна розмірність", "Діапазони alpha (*E-5)");
+            graphWindow.Show();
+
+            //отримуємо сформовані картинки
+            SubsetsWindow subsetsWindow = new SubsetsWindow(fractalDimension.LocalDensityImagesDirectory, fractalDimension.AlphaGroups);
+            subsetsWindow.Show();
+        }
+    }
+}
